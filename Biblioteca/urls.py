f88b0ac5506b8e2130b80d78800f30795c2a986d
keilanyg <keilanydes@gmail.com>
2023-05-27 14:03:14 -0300

    Usando drf_spectacular

--- Biblioteca/urls.py
+++ Biblioteca/urls.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
+    # OPEN API
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # AUTENTICAÇÃO
+    path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    # OUTROS ENDPOINTS
     path('admin/', admin.site.urls),
+    path('livro/', include('livro.urls')),
+    path('auth/', include('usuarios.urls')),
+    
 ]
